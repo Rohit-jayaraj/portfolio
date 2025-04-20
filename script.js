e965517fb7073b3e9c1ffacf69e78931e5107f6e
Rohit Jayaraj <rohit.jayaraj@gmail.com>
2025-04-20 20:43:30 +0530

    Moved skills section up

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const albumSelect = document.getElementById("albumSelect");
     const spotifyPlayer = document.getElementById("spotifyPlayer");
+    
   
     const albumMap = {
       "0UcyqmHNn536yy8boNihhb": "https://open.spotify.com/embed/album/0UcyqmHNn536yy8boNihhb?utm_source=generator&theme=0",
@@ -18,7 +19,8 @@ document.addEventListener("DOMContentLoaded", function () {
       if (selectedValue === "6PFPjumGRpZnBzqnDci6qJ" || selectedValue === "3XB2yloP7l00tEUmaODtVi") {
         albumSelect.style.border = "2px solid white";
       } else {
-        albumSelect.style.border = "2px solid #e50914"; // red by default
+        albumSelect.style.border = "2px solid #e50914"; 
       }
     });
+
 });
